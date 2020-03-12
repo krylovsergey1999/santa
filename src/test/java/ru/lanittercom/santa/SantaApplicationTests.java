ee8016b98a6398e5b81ab36e9ced8d75e22eecaa
Krylov Sergey <kssergeyru@gmail.com>
2020-03-12 20:52:40 +0300

    Initial commit

---
+++ src/test/java/ru/lanittercom/santa/SantaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lanittercom.santa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
